b2be30c6be2f332e7c924dbc6b753079218a939a
Guillaume Sabran <sabranguillaume@gmail.com>
2017-04-06 14:02:36 -0700

    Initial Commit

---
+++ TestAVItemStatus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAVItemStatus
+//
+//  Created by Guillaume Sabran on 4/6/17.
+//  Copyright © 2017 PaperoInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
